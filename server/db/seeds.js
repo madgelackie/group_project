a1483e5f1b308056ad848d42aae0c21a74b60064
Andrew Thomas <ajthomas145@gmail.com>
2021-10-30 13:03:41 +0100

    quiz questions added

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -2,9 +2,186 @@ use solar_system;
 db.dropDatabase();
 
 db.planets.insertMany([
-    {
-        name:"Earth",
-        image:"image URL",
-        description:"These are very interesting facts about the planet"}
+    {solarSystem:
+        {general:
+            {
+
+            }
+
+        },
+
+        {planets:
+            {
+                name:"Earth",
+                image:"image URL",
+                description:"These are very interesting facts about the planet",
+                temperature: "",
+                distanceFromTheSun: "",
+                size:"",
+                funFact:""
+            },
+            {
+                name:"Earth",
+                image:"image URL",
+                description:"These are very interesting facts about the planet",
+                temperature: "",
+                distanceFromTheSun: "",
+                size:"",
+                funFact:""
+            },
+            {
+                name:"Earth",
+                image:"image URL",
+                description:"These are very interesting facts about the planet",
+                temperature: "",
+                distanceFromTheSun: "",
+                size:"",
+                funFact:""
+            },
+            {
+                name:"Earth",
+                image:"image URL",
+                description:"These are very interesting facts about the planet",
+                temperature: "",
+                distanceFromTheSun: "",
+                size:"",
+                funFact:""
+            },
+            {
+                name:"Earth",
+                image:"image URL",
+                description:"These are very interesting facts about the planet",
+                temperature: "",
+                distanceFromTheSun: "",
+                size:"",
+                funFact:""
+            },
+            {
+                name:"Earth",
+                image:"image URL",
+                description:"These are very interesting facts about the planet",
+                temperature: "",
+                distanceFromTheSun: "",
+                size:"",
+                funFact:""
+            },
+            {
+                name:"Earth",
+                image:"image URL",
+                description:"These are very interesting facts about the planet",
+                temperature: "",
+                distanceFromTheSun: "",
+                size:"",
+                funFact:""
+            },
+            {
+                name:"Earth",
+                image:"image URL",
+                description:"These are very interesting facts about the planet",
+                temperature: "",
+                distanceFromTheSun: "",
+                size:"",
+                funFact:""
+            },
+            {
+                name:"Earth",
+                image:"image URL",
+                description:"These are very interesting facts about the planet",
+                temperature: "",
+                distanceFromTheSun: "",
+                size:"",
+                funFact:""
+            }
+        }, 
+
+        {quiz:
+            {
+                question:"Why is Mars red?",
+                answerCorrect:"Iron oxide in the rock",
+                answerWrong1:"Alien’s painted it red",
+                answerWrong2: "It is covered with tomato plants"
+                
+            },
+            {
+                question:"How long is a day on Mars?",
+                answerCorrect:"almost the same as on Earth",
+                answerWrong1:"3 hours long",
+                answerWrong2: "50 hours long"
+            },
+            {
+                question:"Mercury is:",
+                answerCorrect:"The closest planet to the sun",
+                answerWrong1:"The furthest planet from the sun",
+                answerWrong2: "The 5th planet from the sun"
+            },
+            {
+                question:"Which planet is the hottest?",
+                answerCorrect:"Venus",
+                answerWrong1:"Mercury",
+                answerWrong2: "Mars"
+            },
+            {
+                question:"Why is Venus so hot?",
+                answerCorrect:"Because it has a thick atmosphere that keeps the heat from the sun in",
+                answerWrong1:"Because someone left the heating on.",
+                answerWrong2: "Because it is the closest planet to the sun."
+            },
+            {
+                question:"Who are Earth’s nearest neighbours?",
+                answerCorrect:"Venus and Mars",
+                answerWrong1:"Neptune and Venus",
+                answerWrong2: "Saturn and Mars"
+            },
+            {
+                question:"What is the great red spot on Jupiter",
+                answerCorrect:"A storm",
+                answerWrong1:"A volcano",
+                answerWrong2: "An Ocean"
+            },
+            {
+                question:"Saturn is a planet known for the large rings that surround it. What are these rings made of?",
+                answerCorrect:"Ice and rock",
+                answerWrong1:"Space junk",
+                answerWrong2: "Gases"
+            },
+            {
+                question:"Like Saturn, what does Uranus have, but they are hard to see bacause they are dark",
+                answerCorrect:"Rings",
+                answerWrong1:"Oceans",
+                answerWrong2: "Clouds"
+            },
+            {
+                question:"What kind of weather does Neptune have",
+                answerCorrect:"Very cold and windy",
+                answerWrong1:"Very warm",
+                answerWrong2: "The same as Earth"
+            },
+            {
+                question:"Now that Pluto is no longer included, how many planets are there in the Solar System?",
+                answerCorrect:"8",
+                answerWrong1:"6",
+                answerWrong2: "10"
+            },
+            {
+                question:"How many Earths can fit inside the sun?",
+                answerCorrect:"1.3 million",
+                answerWrong1:"5,000",
+                answerWrong2: "250,000"
+            },
+            {
+                question:"What was the first animal to go into orbit?",
+                answerCorrect:"A dog",
+                answerWrong1:"A cat",
+                answerWrong2: "A bird"
+            },
+           
+        }
+
+
+
+    }
+    
+
+
 
 ])
