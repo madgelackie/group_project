e599ac40ab99d03292f92f8522fc0329592ab360
StevenMeiklejohn <stevenmeiklejohn700@gmail.com>
2021-11-01 11:43:43 +0000

    Added popup component

--- client/src/containers/SolarSystemContainer.js
+++ client/src/containers/SolarSystemContainer.js
@@ -1,6 +1,8 @@
 import {useState, useEffect} from 'react'
 import PlanetList from '../components/PlanetList'
 import PlanetDetail from '../components/PlanetDetail'
+import Popup from 'reactjs-popup';
+
 
 
 const baseURL = 'http://localhost:5000/api/planets'
@@ -9,25 +11,32 @@ const baseURL = 'http://localhost:5000/api/planets'
 
 const SolarSystemContainer = () => {
 
-    const [planets, setPlanets] = useState([])
-    const [selectedPlanet, setSelectedPlanet] = useState(null);
-
-    useEffect(() => {
-        return fetch(baseURL)
-        .then(res => res.json())
-        .then(planets => setPlanets(planets))
-    })
 
 
 
-    const onPlanetClick = (planet) => {
-        setSelectedPlanet(planet);
-    }
 
     return (
         <>
-            <PlanetList planets={planets} onPlanetClick={onPlanetClick}/>
-            {selectedPlanet ? <PlanetDetail selectedPlanet= {selectedPlanet}/>:null}
+        <Popup trigger={<img src={"/earth.jpeg"} alt="Earth Image" height={200}/>} modal nested >
+    {close => (
+      <div className="modal">
+        <button className="close" onClick={close}>
+          &times;
+        </button>
+        <div className="header"> Modal Title </div>
+        <div className="content">
+          {' '}
+          Planet Earth
+          <br />
+          This is where (most) of you live.
+        </div>
+        <div className="actions">
+
+        </div>
+      </div>
+    )}
+  </Popup>
+            
 
         </>
     )
