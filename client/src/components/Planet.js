79d84c2d48068f50d249e662ca89c95575199226
Andrew Thomas <ajthomas145@gmail.com>
2021-11-01 16:26:39 +0000

    popup showing data

--- client/src/components/Planet.js
+++ client/src/components/Planet.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Popup from "reactjs-popup";
 
 const Planet = ({planet, onPlanetClick, onPlanetHover, onPlanetLeave}) =>{
     const handleClick = function(){
@@ -15,7 +16,37 @@ const Planet = ({planet, onPlanetClick, onPlanetHover, onPlanetLeave}) =>{
 
     return (
     
-    <li onClick = {handleClick} onMouseEnter={handleHover} onMouseLeave={handleLeave}><img src={planet.image} /></li>)
+    <li 
+    onClick = {handleClick} onMouseEnter={handleHover} onMouseLeave={handleLeave}>
+    <Popup trigger={<img src={planet.image} alt="Earth image" />} modal nested >
+    {close => (
+        <div className="modal">
+        <button classname="close" onClick={close}>
+            &times;
+        </button>
+        <div className="header"> Title </div>
+        <div className="content">
+          {' '}
+          {planet.name}
+          <br />
+         
+          <p>Average Temperature:{planet.avgTemperature}</p>
+          <p>Size(radius):{planet.sizeRadius}</p>
+          <p>Distance From Sun:{planet.distanceFromSun}</p>
+          <p>Length of Day:{planet.lengthOfDay}</p>
+          <p>Length of Year:{planet.lengthOfYear}</p>
+          <p>Number of Moons:{planet.noOfMoons}</p>
+          <p>{planet.funFact}</p>
+        
+        </div>
+        <div className="actions">
+
+        </div>
+      </div>
+    )}
+    </Popup>
+
+    </li>)
     
     
 }
