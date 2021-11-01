93e23b492a107ef2c0394992e74c23d568cc1bfe
Andrew Thomas <ajthomas145@gmail.com>
2021-11-01 12:26:31 +0000

    on hoever enter works

--- client/src/components/Planet.js
+++ client/src/components/Planet.js
@@ -1,13 +1,17 @@
 import React from "react";
 
-const Planet = ({planet, onPlanetClick}) =>{
+const Planet = ({planet, onPlanetClick, onPlanetHover}) =>{
     const handleClick = function(){
         onPlanetClick(planet);
     }
 
+    const handleHover = function(){
+        onPlanetHover(planet);
+    }
+
     return (
     
-    <li onClick = {handleClick}><img src={planet.image} /></li>)
+    <li onClick = {handleClick} onMouseEnter={handleHover}><img src={planet.image} /></li>)
     
     
 }
--- client/src/containers/SolarSystemContainer.js
+++ client/src/containers/SolarSystemContainer.js
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react'
 import PlanetList from '../components/PlanetList'
 import PlanetDetail from '../components/PlanetDetail'
+import PlanetHover from '../components/PlanetHover'
 
 
 const baseURL = 'http://localhost:5000/api/planets'
@@ -11,6 +12,7 @@ const SolarSystemContainer = () => {
 
     const [planets, setPlanets] = useState([])
     const [selectedPlanet, setSelectedPlanet] = useState(null);
+    const [hoveredPlanet, setHoveredPlanet] = useState(null);
 
     useEffect(() => {
         return fetch(baseURL)
@@ -19,15 +21,19 @@ const SolarSystemContainer = () => {
     })
 
 
-
     const onPlanetClick = (planet) => {
         setSelectedPlanet(planet);
     }
 
+    const onPlanetHover = (planet) => {
+        setHoveredPlanet(planet);
+    }
+
     return (
         <>
-            <PlanetList planets={planets} onPlanetClick={onPlanetClick}/>
+            <PlanetList planets={planets} onPlanetClick={onPlanetClick} onPlanetHover={onPlanetHover}/>
             {selectedPlanet ? <PlanetDetail selectedPlanet= {selectedPlanet}/>:null}
+            {hoveredPlanet ? <PlanetHover hoveredPlanet = {hoveredPlanet}/>:null}
 
         </>
     )
