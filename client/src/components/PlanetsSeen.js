fc6371ecff551108e464d04b6e8c2de4177aa885
madgelackie <marionmlackie@gmail.com>
2021-11-02 14:56:26 +0000

    tidied up code

--- client/src/components/PlanetsSeen.js
+++ client/src/components/PlanetsSeen.js
@@ -6,27 +6,6 @@ const PlanetsSeen = ({seenPlanets}) => {
     const seenPlanetsList = seenPlanets.map(planet => {
         return <li id="inline-list" >{planet.name}</li>
     })
-    // const seenPlanetsNoDuplicates = [seenPlanetsList[0]];
-    // for (var i=1; i<seenPlanetsList.length; i++) {
-    //     if (seenPlanetsList[i] != [i-1]) seenPlanetsNoDuplicates.push(seenPlanetsList[i]);
-    //     console.log(seenPlanetsNoDuplicates)
-    // }
-    // const newPlanetsSeenList = seenPlanetsNoDuplicates.map((planet) => {
-    //     return <li id="inline-list" >{planet.name}</li>
-        
-    // })
-    // const seenPlanetsNoDuplicates = (seenPlanetsList) => {
-    //     let planetCompare = [seenPlanetsList[0]]
-    //     for (var i=1; i<seenPlanetsList.length; i++) {
-    //     if (seenPlanetsList[i] != [i-1]) 
-    //     return planetCompare.push(seenPlanetsList[i]);   
-    // }}
-
-    // const newPlanetsSeenList = planetCompare.map((planet) => {
-    //     return <li id="inline-list" >{planet.name}</li>
-
-    // return <li id="inline-list">{planet.name}</li>
-    // const view = console.log(newPlanetsSeenList)
 
     return (
         <>
